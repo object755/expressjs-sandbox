60a3681a776ab209e2b6d48c2e0788eb0d99cc5a
object755 <bfgoodman756@gmail.com>
2023-09-04 21:18:47 +0300

    postAllProfiles API added, removed some comments

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import router from './router.js'
-import { PORT, DB_URL, POST_ALL_PROFILES_PATH, GET_API_URL } from './serverData.js'
+import { PORT, DB_URL, POST_ALL_PROFILES_PATH, CRUD_PATH, GET_API_URL } from './serverData.js'
 import fileUpload from 'express-fileupload'
-import { pilotsManager } from './pilotsManager.js'
-
-// const PORT = 5555;
-// let dbName = `Dapplication`
-
-// // creates path to db parent
-// const DB_URL = `mongodb+srv://${CREDENTIALS.username}:${CREDENTIALS.password}@clustereu.wcfbshc.mongodb.net/${dbName}`;
-// const API_PILOTS_URL = `http://localhost:${PORT}/api/pilots`;
 
 const app = express();
 
@@ -28,44 +20,12 @@ async function startApp() {
         await mongoose.connect(DB_URL, {useUnifiedTopology: true, useNewUrlParser: true})
         app.listen(PORT, () => console.log(`[Server is working]:[Port:${PORT}]`));
 
-        let urlForAllProfilesPosting = GET_API_URL(PORT,POST_ALL_PROFILES_PATH)
-        postAllKnownProfilesToDataBase(urlForAllProfilesPosting)
+        let urlForAllProfilesPosting = GET_API_URL(PORT,POST_ALL_PROFILES_PATH);
+
+        console.log(urlForAllProfilesPosting);
     } catch(e) {
         console.log(e);
     }
-}
-
-function postAllKnownProfilesToDataBase(url) {
-    Object.entries(pilotsManager).forEach(pilot => {
-        let [pilotName, pilotData] = pilot;
-
-        let {country, city, socials} = pilotData
-        
-        postPilotData(url, pilotName, country, city, socials)
-        
-        console.log(`${country} ${city} ${socials}`)
-    })
-}
-
-function postPilotData(url, pilotName, country, city, social, avatar = '') {
-    const data = {
-        pilotName: pilotName,
-        country: city,
-        country: country,
-        social: social,
-        avatar: avatar,
-    };
-
-    fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error("Error:", error))
 }
 
 startApp()
--- router.js
+++ router.js
@@ -7,20 +7,16 @@ import { CRUD_PATH, POST_ALL_PROFILES_PATH } from './serverData.js';
 const router = new Router()
 
 router.post(`/${CRUD_PATH}`, PostController.create)
+router.post(`/${POST_ALL_PROFILES_PATH}`, PostController.create)
 router.get(`/${CRUD_PATH}`, PostController.getAll)
 router.get(`/${CRUD_PATH}/:id`, PostController.getOne)
 router.put(`/${CRUD_PATH}`, PostController.update)
 router.delete(`/${CRUD_PATH}/:id`, PostController.delete)
-// router.route(`/${POST_ALL_PROFILES_PATH}`)
-//   .get(postAllProfilesToDataBase) // Execute the function for GET requests
-// //   .post(postAllProfilesToDataBase);
+router.get(`/${POST_ALL_PROFILES_PATH}`, (req, res) => {
 
-// router.get(`/${POST_ALL_PROFILES_PATH}`, (req, res) => {
-    
-//     postAllProfilesToDataBase(req, res);
-    
-//     // You can send a response to the client
-//     res.send('Function executed successfully!');
-//   });
+    postAllProfilesToDataBase();
+
+    res.send('Function executed successfully!');
+});
 
 export default router;
