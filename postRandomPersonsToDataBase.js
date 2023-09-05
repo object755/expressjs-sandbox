a76365464c0675a35a4cec9cd89405b2ba01e291
object755 <bfgoodman756@gmail.com>
2023-09-05 17:25:59 +0300

    postRandomPersons function added (wip)

--- postRandomPersonsToDataBase.js
+++ postRandomPersonsToDataBase.js
@@ -2,24 +2,8 @@ import { PORT, POST_ALL_PROFILES_PATH, GET_API_URL } from "./serverData.js";
 import { pilotsManager } from "./pilotsManager.js";
 
 let postUrl = GET_API_URL(PORT, POST_ALL_PROFILES_PATH);
-console.log(`----------------------------------------------------`)
-console.log(`VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV`)
 
-export default function postAllProfilesToDataBase() {
-    // for (let i = 0; i < 3; i++) {
-    //     dbCollectionName = dbCollectionName+1
-        
-    //     Object.entries(pilotsManager).forEach(pilot => {
-    //         let [pilotName, pilotData] = pilot;
-    
-    //         let {country, city, socials} = pilotData
-            
-    //         postProfileData(pilotName, country, city, socials)
-            
-    //         console.log(`${country} ${pilotName} ${city} ${socials}`)
-    //     }) 
-    // }
-   
+export default function postRandomPersonsToDataBase() {   
     Object.entries(pilotsManager).forEach(pilot => {
         let [pilotName, pilotData] = pilot;
 
--- router.js
+++ router.js
@@ -2,20 +2,25 @@ import Router from 'express'
 import PostController from './PostController.js'
 import postAllProfilesToDataBase from './postAllProfilesToDataBase.js'
 import { CRUD_PATH, POST_ALL_PROFILES_PATH } from './serverData.js';
+import postRandomPersonsToDataBase from './postRandomPersonsToDataBase.js';
 
 
 const router = new Router();
 
 router.post(`/${CRUD_PATH}`, PostController.create)
-router.post(`/${POST_ALL_PROFILES_PATH}`, PostController.create)
 router.get(`/${CRUD_PATH}`, PostController.getAll)
 router.get(`/${CRUD_PATH}/:id`, PostController.getOne)
 router.put(`/${CRUD_PATH}`, PostController.update)
 router.delete(`/${CRUD_PATH}/:id`, PostController.delete)
+
 router.get(`/${POST_ALL_PROFILES_PATH}`, (req, res) => {
-
     postAllProfilesToDataBase();
+    res.send('All known profiles arr uploaded to database');
+});
 
+router.get(`/${POST_ALL_PROFILES_PATH}`, (req, res) => {
+    let quantity = 
+    postRandomPersonsToDataBase();
     res.send('Function executed successfully!');
 });
 
